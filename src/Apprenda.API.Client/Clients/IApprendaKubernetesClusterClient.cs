ad0502fc6e4da1d956049e299b269f3676466cf7
Mathieson Sterling <gsterling@apprenda.com>
2017-11-28 04:31:18 -0500

    Added Kubernetes cluster management for SOC API

---
+++ src/Apprenda.API.Client/Clients/IApprendaKubernetesClusterClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApprendaAPIClient.Models;
+using ApprendaAPIClient.Models.SOC.Kubernetes;
+
+namespace ApprendaAPIClient.Clients
+{
+    public interface IApprendaKubernetesClusterClient
+    {
+        Task<IEnumerable<KubernetesCluster>> GetAllKubernetesClusters();
+        Task<KubernetesCluster> GetKubernetesCluser(string clusterName);
+
+        Task<KubernetesClusterReportCard> AddKubernetesCluster(KubernetesCluster cluster);
+        Task<KubernetesClusterReportCard> UpdateKubernetesCluster(KubernetesCluster cluster);
+
+        Task<bool> DeleteKubernetesCluster(string clusterName);
+
+        Task<KubernetesClusterReportCard> ValidateKubernetesCluster(string clusterName);
+    }
+}
---
+++ src/Apprenda.API.Client/Models/SOC/Kubernetes/KubernetesCluster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprendaAPIClient.Models.SOC.Kubernetes
+{
+    public class KubernetesCluster
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int CloudId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string GatewayHostnames { get; set; }
+        public string HeapsterUrl { get; set; }
+        public string ClientCertificate { get; set; }
+        public string ClientCertificatePassword { get; set; }
+        public string AuthenticationMode { get; set; }
+    }
+}
