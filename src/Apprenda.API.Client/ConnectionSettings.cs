6efb26641d7d4d3d50608cd1db1f67178335ec65
Mathieson Sterling <gsterling@apprenda.com>
2017-11-28 05:43:00 -0500

    Added empty ConnectionSettings implementation for easy consumption in powershell scripting

---
+++ src/Apprenda.API.Client/ConnectionSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApprendaAPIClient
+{
+    public class ConnectionSettings : IConnectionSettings
+    {
+        public string AppsUrl { get; set; }
+        public IUserLogin UserLogin { get; set; }
+    }
+}
