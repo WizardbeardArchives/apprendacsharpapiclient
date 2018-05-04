43eb48ad8e3a95447225b346ffb5a5672cff7c17
Unknown <unknown@example.com>
2018-05-04 13:40:29 +0200

    Got working!

--- src/apprendacsharpcoreapiclient/HttpClientExtensions.cs
+++ src/apprendacsharpcoreapiclient/HttpClientExtensions.cs
@@ -11,14 +11,19 @@ namespace apprendacsharpcoreapiclient
     /// </summary>
     public static class HttpClientExtensions
     {
+        private static StringContent MakeStringContent<T>(T item)
+        {
+            return new StringContent(JsonConvert.SerializeObject(item), Encoding.UTF8, "application/json");
+        }
+
         public static Task<HttpResponseMessage> PutAsJsonAsync<T>(this HttpClient client, string uri, T item)
         {
-            return client.PutAsync(uri, new StringContent(JsonConvert.SerializeObject(item)));
+            return client.PutAsync(uri, MakeStringContent(item));
         }
 
         public static Task<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, string uri, T item)
         {
-            return client.PostAsync(uri, new StringContent(JsonConvert.SerializeObject(item)));
+            return client.PostAsync(uri, MakeStringContent(item));
         }
 
         public static async Task<TResp> ReadAsAsync<TResp>(this HttpContent content)
