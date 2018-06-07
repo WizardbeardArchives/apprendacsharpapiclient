11f916ac14ba4569e8ceed33f8f598a377837710
Erik L <eriklg@gmail.com>
2018-06-07 14:17:06 -0400

    There was an issue serializing the response after adding a k8s cluster. Fixed the setter for the internal objects. Added test to add a k8s cluster.

--- src/Apprenda.Testing.RestAPITests/Tests/SOCTests/KubernetesTests.cs
+++ src/Apprenda.Testing.RestAPITests/Tests/SOCTests/KubernetesTests.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Apprenda.Testing.RestAPITestTools.ValueItems;
 using Xunit;
 using Xunit.Abstractions;
+using ApprendaAPIClient.Models.SOC.Kubernetes;
 
 namespace Apprenda.Testing.RestAPITests.Tests.SOCTests
 {
@@ -35,6 +36,29 @@ namespace Apprenda.Testing.RestAPITests.Tests.SOCTests
 
                     Assert.NotNull(validation);
                 }
+            }
+        }
+        [Fact]
+        public async Task CreateKubernetesCluster()
+        {
+            using (var session = await StartSession())
+            {
+                var client = await session.GetClient(ApiPortals.SOC);
+                var newCluster = new KubernetesCluster()
+                {
+                    AuthenticationMode = "BasicAuth",
+                    GatewayHostnames = "K8SPUBLIC787K01",
+                    CloudId = 1,
+                    Name = "k8spublic",
+                    Url = "https://K8SPUBLIC787K01:6443",
+                    Username = "admin",
+                    Password = "Ziyzalocdotoy5722"
+                };
+                var addition = client.AddKubernetesCluster(newCluster).Result;
+
+                var addedCluster = client.GetKubernetesCluser("k8spublic").Result;
+                Assert.Equal(newCluster.Name, addedCluster.Name);
+
             }
         }
     }
